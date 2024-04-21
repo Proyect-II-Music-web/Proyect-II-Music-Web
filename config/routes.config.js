74308e09c269b8c5f145ace20e675768e10d3c36
Diego Guaman <diegoguaman05@hotmail.com>
2024-04-21 18:11:28 +0200

    feat: band resgister

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,4 +1,14 @@
 const router= require("express").Router();
+const { 
+    register,
+    doRegister
+} = require("../controllers/band.controller");
 
 router.get("/", (req, res, next) => res.render("home"));
+
+//Band
+router.get("/register", register);
+router.post("/register", doRegister);
+
 module.exports = router;
+
---
+++ controllers/band.controller.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Band = require("../models/Band.model");
+
+module.exports.register = (req, res, next) => {
+    res.render("register");
+}
+module.exports.doRegister = (req, res, next) => {
+    const renderWithErrors = (errors, values)=>{
+        res.render("register", {errors, values});
+    }
+    console.log(req.body)
+    Band.create(req.body)
+
+        .then((band) => {
+          console.log(band);
+          res.redirect("/login")  
+        })
+        .catch((err) => {
+          if (err instanceof mongoose.Error.ValidationError) {
+            renderWithErrors(err.errors, req.body);
+          } else {
+            next();
+          } 
+        })
+        
+}
