9fd0ab511e4dace2b779af861a0e620f5bcbc8e2
Diego Guaman <diegoguaman05@hotmail.com>
2024-04-21 12:56:24 +0200

    feat: Model to register band ready

--- models/Band.model.js
+++ models/Band.model.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const { REQUIRED_FIELD_ERROR } = require("../constants/errorMessages");
 const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const SALT_ROUNDS = 10;
 const bandSchema = mongoose.Schema({
     name: {
         type: String,
@@ -14,8 +16,28 @@ const bandSchema = mongoose.Schema({
         unique: true,
         trim: true,
         match: [EMAIL_REGEX, "Introduce un email válido"]
+    },
+    password: {
+        type: String,
+        required: [true, REQUIRED_FIELD_ERROR],
+        minLength: [8, "La contraseña dete tener al menos 8 caracteres"]
     }
 });
 
+//Hasheamos la contraseña antes de guardarla
+bandSchema.pre("save", function (next) {
+    if (this.isModified("password")) {
+        bcrypt.hash(this.password, SALT_ROUNDS)
+            .then(hash=> {
+                this.password = hash;
+            })
+    } else{
+        next();
+    }
+})
+
+bandSchema.methods.checkPassword = function (passwordToCompare) {
+   return bcrypt.compare(passwordToCompare, this.password) 
+}
 const Band = mongoose.model("Band", bandSchema);
 module.exports = Band;
