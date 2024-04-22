43b0e1bb40b7bb6f87e58e010d1acd80f7844187
Diego Guaman <diegoguaman05@hotmail.com>
2024-04-22 19:54:53 +0200

    feat: error validation band register

--- controllers/band.controller.js
+++ controllers/band.controller.js
@@ -5,22 +5,19 @@ module.exports.register = (req, res, next) => {
   res.render("register");
 }
 module.exports.doRegister = (req, res, next) => {
-    //const renderWithErrors = (errors, values) => {
-    //   res.render("register", {errors, values});
-    //}
-    console.log('^^^^^', req.body);
+    const renderWithErrors = (errors, values) => {
+       res.render("register", {errors, values});
+    }
     Band.create(req.body)
       .then((band) => {
-        console.log(req.body)
-        console.log(band);
         res.redirect("/login");
       })
       .catch((err) => {
         console.log(err);
-        //if (err instanceof mongoose.Error.ValidationError) {
-        //  renderWithErrors(err.errors, req.body);
-        //} else {
-        //  next();
-        //} 
+        if (err instanceof mongoose.Error.ValidationError) {
+          renderWithErrors(err.errors, req.body);
+        } else {
+          next();
+        } 
       });
 }
