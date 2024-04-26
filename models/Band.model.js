307468267ca639ad41a213642877de0205cfff68
JohnJesusMendezC <jrmc1110@gmail.com>
2024-04-26 21:51:43 +0200

    feat: band-model

--- models/Band.model.js
+++ models/Band.model.js
@@ -1,44 +1,28 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-
 const { REQUIRED_FIELD_ERROR } = require("../constants/errorMessages");
-const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-const SALT_ROUNDS = 10;
 const bandSchema = mongoose.Schema({
     name: {
         type: String,
         trim: true,
         required: [true, REQUIRED_FIELD_ERROR]
     },
-    email: {
+
+    description: {
         type: String,
-        required: [true, REQUIRED_FIELD_ERROR],
-        unique: true,
-        trim: true,
-        match: [EMAIL_REGEX, "Introduce un email válido"]
+        required: [true, REQUIRED_FIELD_ERROR]
     },
-    password: {
-        type: String,
-        required: [true, REQUIRED_FIELD_ERROR],
-        minLength: [8, "La contraseña dete tener al menos 8 caracteres"]
+
+    tags: {
+        type: [String],
+        required: [true, REQUIRED_FIELD_ERROR]
+    },
+
+    members: {
+        type: [String],
+        required: [true, REQUIRED_FIELD_ERROR]
     }
+
 });
 
-//Hasheamos la contraseña antes de guardarla
-bandSchema.pre("save", function (next) {
-    if (this.isModified("password")) {
-        bcrypt.hash(this.password, SALT_ROUNDS)
-            .then(hash=> {
-                this.password = hash;
-                next();
-            })
-    } else{
-        next();
-    }
-})
-
-bandSchema.methods.checkPassword = function (passwordToCompare) {
-   return bcrypt.compare(passwordToCompare, this.password) 
-}
 const Band = mongoose.model("Band", bandSchema);
 module.exports = Band;
