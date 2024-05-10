0165b1548d715e7ff564857b01f8ce12dcc54fb7
Diego Guaman <diegoguaman05@hotmail.com>
2024-05-10 19:20:42 +0200

    feat: home post sugestion, restrict home route

--- controllers/misc.controller.js
+++ controllers/misc.controller.js
@@ -1,9 +1,20 @@
-const Post = require("../models/Post.model")
+const Post = require("../models/Post.model");
 module.exports.getHome = (req, res, next) => {
-    Post.find()
-        .then((posts) => {
-            res.render("home", {posts})
+  // Obtener la fecha actual y la fecha un mes después
+  const currentDate = new Date();
+  const oneMonthLater = new Date(currentDate);
+  oneMonthLater.setMonth(oneMonthLater.getMonth() + 6);
 
-        })
-        .catch((err) => next(err))
-}
+  // Realizar la consulta para encontrar eventos entre la fecha actual y un mes después
+  Post.find({
+    date: {
+      $gte: currentDate,
+      $lte: oneMonthLater,
+    },
+    isClosed: true
+  })
+    .then((posts) => {
+      res.render("home", { posts });
+    })
+    .catch((err) => next(err));
+};
